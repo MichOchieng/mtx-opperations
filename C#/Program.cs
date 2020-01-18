9b7c195893a23ccf5b90f07a6187644a5ab15c2b
MichOchieng <MichOchieng@gmail.com>
2020-01-17 23:27:04 -0800

    Added C# version of java code

---
+++ C#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = System.Console;
+
+namespace ConsoleApp1
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            double[,] arrSm = new double[20,20];         
+            double[,] arrMd = new double[200,200]; 
+            double[,] arrLg = new double[2000,2000];
+
+            q1 x = new q1(arrSm);
+            x.colTest();
+            x.rowTest();
+            
+        }
+}
+}
